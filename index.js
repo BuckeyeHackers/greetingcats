a988009d10c1446681969d4528040ab035cae0d7
pascalrascal <marchetti.14@osu.edu>
2016-10-08 03:53:25 -0400

    Added in Directory and Express Static
    reafuodsa

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 var express = require('express');
 var fs = require('fs');
+var cardController = require('/controllers/card');
 
 var app = express();
 
-app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/scripts'));
+app.use(express.static(__dirname + '/assets'));
 
 
 app.get('/', function (req, res){ 
-    res.sendFile("/index.html");
+    res.render("index");
 });
 
 app.post('/card/create', function(req, res){
-    
+
 });
+
+app.get('/card/img360/list', function(req, res){
+
+}
 
 var port = 9000;
 app.listen(port);
