eef6f0e5f31695c94022498cdcfd674fdf8ead6b
pascalrascal <marchetti.14@osu.edu>
2016-10-08 01:40:54 -0400

    Moved index to views
    renders view

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var fs = require('fs');
+
 var app = express();
 
-app.get('/', function (req, res){ res.send('Hello there, world!\n');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/scripts'));
+
+
+app.get('/', function (req, res){ 
+    res.sendFile("/index.html");
+});
+
+app.post('/card/create', function(req, res){
+    
 });
 
 var port = 9000;
 app.listen(port);
 console.log('Listening on port', port);
+ 
